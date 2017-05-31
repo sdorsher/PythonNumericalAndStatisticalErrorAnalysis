a77f97bea8db24241f22ba7e69964a0125e75476
Steven Dorsher <sdorsh1@lsu.edu>
2017-05-31 10:50:39 -0500

    fits the function Peter wants fit (no constant offset, no power law) and uses -1 and -2 power law scaling on weights in addition to no scaling

--- plotfit2.py
+++ plotfit2.py
@@ -4,22 +4,20 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
 from scipy import optimize
 
-def fit_func(ldata, param0,param1,param2,param3,param4):
-    psir=param0+param1/(2*ldata-1)/(2*ldata+3)+param2/(2*ldata-3)/(2*ldata-1)/(2*ldata+3)/(2*ldata+5)+param3/(2*ldata-5)/(2*ldata-3)/(2*ldata-1)/(2*ldata+1)/(2*ldata+3)/(2*ldata+5)/(2*ldata+7)+param4/(2*ldata-5)/(2*ldata-3)/(2*ldata-1)/(2*ldata+1)/(2*ldata+3)/(2*ldata+5)/(2*ldata+7)/(2*ldata-7)/(2*ldata+9)
+def fit_func(ldata, param1,param2,param3,param4):
+    psir=param1/(2*ldata-1)/(2*ldata+3)+param2/(2*ldata-3)/(2*ldata-1)/(2*ldata+3)/(2*ldata+5)+param3/(2*ldata-5)/(2*ldata-3)/(2*ldata-1)/(2*ldata+1)/(2*ldata+3)/(2*ldata+5)/(2*ldata+7)+param4/(2*ldata-5)/(2*ldata-3)/(2*ldata-1)/(2*ldata+1)/(2*ldata+3)/(2*ldata+5)/(2*ldata+7)/(2*ldata-7)/(2*ldata+9)
     return psir
 
-def fit_func2(ldata, param0,param1,param2):
-    psir=param0+param1/(2*ldata-1)/(2*ldata+3)+param2/(2*ldata-3)/(2*ldata-1)/(2*ldata+3)/(2*ldata+5)
+def fit_func2(ldata,param1,param2,param3):
+    psir=param1/(2*ldata-1)/(2*ldata+3)+param2/(2*ldata-3)/(2*ldata-1)/(2*ldata+3)/(2*ldata+5)+param3/(2*ldata-5)/(2*ldata-3)/(2*ldata+3)/(2*ldata+5)/(2*ldata+7)
     return psir
     
-def test_func(ldata,C,A):
-    return C*ldata**-A
 
 finfcolumn=4
 lcolumn=1
 nummodes=31
 
-startmode=2
+startmode=8
 datatable =np.loadtxt("coeffsbyl570.csv", skiprows=startmode)
 
 t0 = datatable[0,0]
@@ -28,21 +26,26 @@ psir=datatable[:,finfcolumn]
 
 errscale=np.zeros(len(llist))
 for ii in range(len(llist)):
-    errscale[ii]=llist[ii]**-2.
+    errscale[ii]=llist[ii]**-1.
+errscale2=np.zeros(len(llist))
+for ii in range(len(llist)):
+    errscale2[ii]=llist[ii]**-2.
+    
+paramopt, paramcov = optimize.curve_fit(fit_func2, llist,psir,sigma=errscale)
+paramopt2, paramcov2=optimize.curve_fit(fit_func2,llist,psir)
+paramopt3, paramcov3 = optimize.curve_fit(fit_func2, llist,psir,sigma=errscale2)
 
-paramopt, paramcov = optimize.curve_fit(fit_func, llist,psir,sigma=errscale)
-linearparam, linearcov = optimize.curve_fit(test_func, llist, psir,sigma=errscale)
 
-
-
-plt.plot(llist,psir,'x',label='Extrapolated data')
-plt.plot(llist,fit_func(llist,paramopt[0],paramopt[1],paramopt[2], paramopt[3], paramopt[4]),'-', label='Fit using Hesthaven et al')
-plt.plot(llist,test_func(llist,linearparam[0], linearparam[1]), '--', label='Power law fit, exp=-'+str(linearparam[1]))
+plt.plot(llist,psir,'x',label='Data, extrapolated to infinite DG order')
+plt.plot(llist,fit_func2(llist,paramopt[0],paramopt[1],paramopt[2]),'-', label='3 term fit, without scaled weignts')
+plt.plot(llist,fit_func2(llist,paramopt2[0],paramopt2[1],paramopt2[2]),'-', label='3 term fit, with sigma~l^-1')
+plt.plot(llist,fit_func2(llist,paramopt3[0],paramopt3[1],paramopt3[2]),'-', label='3 term fit, with sigma~l^-2')
 ax=plt.gca()
+#plt.axis([0.1,100,1e-7,1e-4])
 ax.set_yscale('log')
 ax.set_xscale('log')
 plt.legend(loc='upper right')
 plt.ylabel('Re(dpsi/dr)')
 plt.xlabel('l mode')
-plt.title('Radial self force at infinite DG order, t=' + str(t0)+ ', starting from l=' + str(startmode))
+plt.title('Radial self force, t=' + str(t0)+ ', starting from l=' + str(startmode))
 plt.show()
