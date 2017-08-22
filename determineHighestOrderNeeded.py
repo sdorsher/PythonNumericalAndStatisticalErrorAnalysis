75691376dee0950b1aa4551649cacf004de0dde7
Steven Dorsher <sdorsh1@lsu.edu>
2017-08-22 17:53:42 -0500

    script to determine the highest order needed. Should show exponential convergence to a constant. doesnt. has constant offset. subtracts finf from each DG order, summed over all the modes up to some lmax. may have issues related to conventsion with lsmax or related to the need to extend the sum to get a zero offset.

---
+++ determineHighestOrderNeeded.py
@@ -0,0 +1,54 @@
+from math import *
+import numpy as np
+import scipy.optimize as optimization
+import matplotlib.pyplot as plt
+import csv
+import pylab
+
+def zerooffsetexpdecay(x,alpha,coeff):
+    return coeff*exp(-alpha*x)
+
+datatablefiniteorder=np.loadtxt('genrawsummodes2.csv')
+datatablefinf=np.loadtxt('selfForceOverTime_fit2.csv')
+summedfinfcolumn = 2 #column for finf with unextrapolated sum
+tcolumn=0
+t0=390
+psirfinfsummed=0
+tindex=0
+#assume times match in both files
+for index in range(len(datatablefinf[:,summedfinfcolumn])):
+    #print index, abs(datatablefinf[index,tcolumn]-t0)
+    if abs(datatablefinf[index,tcolumn]-t0)<0.01:
+        #print index, tcolumn, t0
+        tindex=index
+        psirfinfsummed=datatablefinf[tindex,summedfinfcolumn]
+#print t0, tindex, psirfinfsummed
+
+absoluteerr=np.zeros([8])
+for order in range(len(datatablefiniteorder[tindex,1:])):
+    absoluteerr[order]=abs(psirfinfsummed-datatablefiniteorder[tindex,order+1])
+abserr=absoluteerr.reshape(1,8)
+#logabserr=np.log10(abserr)
+orders=np.array([range(16,48,4)])
+#print absoluteerr.shape
+#print orders.shape
+
+
+orders2=orders.flatten()
+abserr2=abserr.flatten()
+#popt,pcov=optimization.curve_fit(zerooffsetexpdecay, orders2, abserr2)
+
+plt.xlabel("DG order")
+plt.ylabel("Absolute error")
+plt.title("|Psir(DG order)-Psir(Finf)| exponentially converges, no l-mode fit")
+#plt.semilogy('log')
+plt.plot(orders2,abserr2,'o')
+#plt.plot(orders,popt[0]*np.exp(-popt[1]*orders),'x')
+#t = np.arange(0.01, 20.0, 0.01)
+#t = np.arange(16, 44, 4)
+#plt.semilogy(t, np.exp(-t/500.))
+#print orders.flatten()
+#plt.semilogy(orders2, np.exp(-orders2/500.))
+plt.grid(True)
+plt.show()
+
